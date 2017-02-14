27510675e1ac7ff0a716562d4259ff3828fdd481
Hassan Zaheer <hasan.coldfire@gmail.com>
2017-02-14 23:30:22 +1100

    added unit tests, reducer, action creators etc

--- voting-client/test/reducer_spec.js
+++ voting-client/test/reducer_spec.js
@@ -12,20 +12,17 @@ describe('reducer', () => {
             state: Map({
                 vote: Map({
                     pair: List.of('Trainspotting', '28 Days Later'),
-                    tally: Map({
-                        Trainspotting: 1
-                    })
+                    tally: Map({ Trainspotting: 1 })
                 })
             })
         };
+        
         const nextState = reducer(initialState, action);
 
         expect(nextState).to.equal(fromJS({
             vote: {
                 pair: ['Trainspotting', '28 Days Later'],
-                tally: {
-                    Trainspotting: 1
-                }
+                tally: { Trainspotting: 1 }
             }
         }));
     });
@@ -76,4 +73,67 @@ describe('reducer', () => {
         }));
     });
 
-})
+    it ('handles VOTE by setting hasVoted', () => {
+        const state = fromJS({
+            vote: {
+                pair: ['Trainspotting', '28 Days later'],
+                tally: {Trainspotting: 1}
+            }
+        });
+        const action = {type: 'VOTE', entry: 'Trainspotting'};
+        const nextState = reducer(state, action);
+
+        expect(nextState).to.equal(fromJS({
+            vote: {
+                pair: ['Trainspotting', '28 Days later'],
+                tally: {Trainspotting: 1}
+            },
+            hasVoted: 'Trainspotting'
+        }));
+    });
+
+    it ('does not set hasVoted for VOTE on invalid entry', () => {
+        const state = fromJS({
+            vote: {
+                pair: ['Trainspotting', '28 Days later'],
+                tally: {Trainspotting: 1}
+            }
+        });
+        const action = {type: 'VOTE', entry: 'Sunshine'};
+        const nextState = reducer(state, action);
+
+        expect(nextState).to.equal(fromJS({
+            vote: {
+                pair: ['Trainspotting', '28 Days later'],
+                tally: {Trainspotting: 1}
+            }
+        }));
+    });
+
+    it ('removes hasVoted on SET_STATE if pair changes', () => {
+        const initialState = fromJS({
+            vote: {
+                pair: ['Trainspotting', '28 Days later'],
+                tally: {Trainspotting: 1}
+            },
+            hasVoted: 'Trainspotting'
+        });
+        const action = {
+            type: 'SET_STATE',
+            state: {
+                vote: {
+                    pair: ['Sunshine', 'Slumdog Millionaire']
+                }
+            }
+        };
+
+        const nextState = reducer(initialState, action);
+
+        expect(nextState).to.equal(fromJS({
+            vote: {
+                pair: ['Sunshine', 'Slumdog Millionaire']
+            }
+        }));
+    });
+
+});
